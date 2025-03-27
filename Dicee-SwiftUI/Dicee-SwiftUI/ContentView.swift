de54043e03aad949c2bdb2ded9ad3d11efc6c86c
ApurvaGaikwad <apurvagaikwad123@gmail.com>
2025-03-28 00:01:49 +0530

    Dice Roll Project in Swift UI with State Property in struct

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Apurva Gaikwad on 27/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1 
+    @State var rightDiceNumber = 1
+
+    var body: some View {
+        ZStack {
+            Image("GreenBackground").resizable().edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("DiceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                    
+                } label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+
+            }
+            
+            
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ Dicee-SwiftUI/Dicee-SwiftUITests/Dicee_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUITests.swift
+//  Dicee-SwiftUITests
+//
+//  Created by Apurva Gaikwad on 27/03/25.
+//
+
+import Testing
+@testable import Dicee_SwiftUI
+
+struct Dicee_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
